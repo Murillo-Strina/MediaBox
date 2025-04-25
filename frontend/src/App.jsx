4c0e819c3f88a2124cb69c79b062401354118575
Murillo-Strina <143843776+Murillo-Strina@users.noreply.github.com>
2025-04-25 02:19:42 -0300

    debug: Route erro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { Routes, Route, BrowserRouter, Navigate} from 'react-router-dom'
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
-import './styles/Vars.module.css';  
+import './styles/Vars.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/MediaBox">
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
